9ab1ae545e7d4208a681956ca549a7875e2c8069
Brandon Hoskins <branjames117@gmail.com>
2022-02-21 21:02:08 -0600

    added gallery and photolist components as well as tests

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,14 @@
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import PhotoList from '../PhotoList';
+
+export default function Gallery({ currentCategory }) {
+  const { name, description } = currentCategory;
+
+  return (
+    <section>
+      <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList category={name} />
+    </section>
+  );
+}
